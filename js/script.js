b35b2651bf82bdd5da77981a36b11fc31391e74b
Maciej Sznurawa <msznurawa@gmail.com>
2019-11-21 12:08:28 +0100

    Change some mobile menu scroll behaviour

--- js/script.js
+++ js/script.js
@@ -1,8 +1,47 @@
 'use strict';
 
+function click (target, callback) {
+  target.addEventListener('click', (e) => {
+  e.preventDefault();
+  return callback(e)
+});
+}
+
+const scrollBelowLogo = () => {
+  window.scrollBy(0, -60);
+};
+
+const hideMobileMenu = () => {
+  mobileMenu.classList.toggle('mobile-hidden');
+};
+
 const hamburgerButton = document.querySelector('.hamburger-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 hamburgerButton.addEventListener('click', (e) => {
-  mobileMenu.classList.toggle('mobile-hidden');
+  hideMobileMenu();
 });
+
+const mobileMenuItems = document.querySelectorAll('.menu__list-item a');
+const documentSections = document.querySelectorAll('*[id^="section"]');
+const documentSectionsArray = [...documentSections];
+
+const scrollToSection = (i) => {
+
+  documentSectionsArray[i].scrollIntoView()
+  //.then(scrollBelowLogo());
+
+};
+
+mobileMenuItems.forEach((item, index) => {
+  click(item, () => {
+    scrollToSection(index);
+    hideMobileMenu();
+  })
+});
+
+
+// mobileMenuItems.forEach(addEventListener('click', (e) => {
+//   mobileMenu.classList.toggle('mobile-hidden');
+// }));
+
