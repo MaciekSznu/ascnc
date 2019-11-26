b3ed7229af94bf7333800206930a187b4c98ea83
Maciej Sznurawa <msznurawa@gmail.com>
2019-11-26 21:02:32 +0100

    Add new fotos and try to fix portfolio slider issue

---
+++ js/sliders.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const teamSlider = new Siema({
+  selector: '.team-wrapper',
+  loop: true,
+});
+
+const portfolioSlider = new Siema({
+  selector: '.portfolio-items-wrapper',
+  // loop: true,
+  // onInit: () => {
+  //   const slider = document.querySelector('.portfolio-items-wrapper');
+  //   const sliderGeneratedContainer = slider.children;
+  //   const generatedSlide = sliderGeneratedContainer.child;
+  //   //get slider width
+  //   //set change position of generatedcontainer by 50%
+
+  // }
+});
+
+// import { tns } from '../node_modules/tiny-slider/src/tiny-slider.js';
+
+// const protfolioSlider = tns({
+//   container: '.portfolio-items-wrapper',
+//   items: 1,
+// });
