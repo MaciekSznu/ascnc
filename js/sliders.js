29611392f33595855260d3543e188bf65a996873
Maciej Sznurawa <msznurawa@gmail.com>
2019-11-28 18:55:36 +0100

    Change team members fotos

--- js/sliders.js
+++ js/sliders.js
@@ -1,48 +1,41 @@
 'use strict';
 
 const width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-
 const mediaTabletPortraitWidth = 768;
 const mediaTabletLandscapeWidth = 1024;
 
-
 const teamSlider =
   (width < mediaTabletPortraitWidth) ?
-      new Siema({
-        selector: '.team-wrapper',
-        loop: true,
-      }) :
+  new Siema({
+    selector: '.team-wrapper',
+    loop: true,
+  }) :
   (width >= mediaTabletPortraitWidth && width <= mediaTabletLandscapeWidth) ?
-      new Siema({
-        selector: '.team-wrapper',
-        loop: true,
-        perPage: 2,
-      }) :
-      new Siema({
-        selector: '.team-wrapper',
-        loop: true,
-        perPage: 4,
-      });
-
-// const portfolioSlider = new Siema({
-//   selector: '.portfolio-items-wrapper',
-//   loop: true,
-// });
+  new Siema({
+    selector: '.team-wrapper',
+    loop: true,
+    perPage: 2,
+  }) :
+  new Siema({
+    selector: '.team-wrapper',
+    loop: true,
+    perPage: 4,
+  });
 
 const portfolioSlider =
   (width < mediaTabletPortraitWidth) ?
-      new Siema({
-        selector: '.portfolio-items-wrapper',
-        loop: true,
-      }) :
+  new Siema({
+    selector: '.portfolio-items-wrapper',
+    loop: true,
+  }) :
   (width >= mediaTabletPortraitWidth && width <= mediaTabletLandscapeWidth) ?
-      new Siema({
-        selector: '.portfolio-items-wrapper',
-        loop: true,
-        perPage: 2,
-      }) :
-      new Siema({
-        selector: '.portfolio-items-wrapper',
-        loop: true,
-        perPage: 2,
-      });
+  new Siema({
+    selector: '.portfolio-items-wrapper',
+    loop: true,
+    perPage: 2,
+  }) :
+  new Siema({
+    selector: '.portfolio-items-wrapper',
+    loop: true,
+    perPage: 2,
+  });
